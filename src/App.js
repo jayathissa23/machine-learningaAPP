62465895909e33f2d04ce071f8ec9105d859670d
NIRMITHA <jayathissaara@gmail.com>
2021-07-02 12:00:55 +0530

    onchange and onclick function

--- src/App.js
+++ src/App.js
@@ -3,17 +3,55 @@ import Navigation from './components/Navigatiohn/Navigation';
 import Logo from './components/logo/Logo'
 import ImgLinkform from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
+import React from 'react';
 
-function App() {
+
+const particleOption={
+  particles: {
+    number: {
+      value:105,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  
+}
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      input:'',
+    }
+  }
+
+  onInputChange=(e)=>{
+    console.log(e.target.value);
+  }
+
+  onButtonSubmit=()=>{
+    console.log(`click`);
+  }
+
+  render(){
   return (
     <div className="App">
-      <Navigation />
-       <Logo />
-       <Rank />
-       <ImgLinkform />
-              {/*<FaceRecognition /> */}
-    </div>
+    <Particles
+        className='particles'
+        params={particleOption}
+        />
+    <Navigation />
+      <Logo />
+      <Rank />
+      <ImgLinkform onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+            {/*<FaceRecognition /> */}
+  </div>
   );
+}
 }
 
 export default App;
