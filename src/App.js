6c8c94234c493bae9182f1e75cc87636e197dd07
NIRMITHA <jayathissaara@gmail.com>
2021-07-02 09:13:31 +0530

    last create rank

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Navigation from './components/Navigatiohn/Navigation';
 import Logo from './components/logo/Logo'
+import ImgLinkform from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
        <Logo />
-              {/*<imgLinkform />
-              <FaceRecognition /> */}
+       <Rank />
+       <ImgLinkform />
+              {/*<FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ImageLinkForm=function(){
+    return (
+        <div>
+           <div className='white f3'>
+                {'Andrei, your currents ranks is...'}
+           </div>
+           <div className='white f1'>
+                {'#5'}
+           </div>
+        </div>
+    )
+}
+
+export default ImageLinkForm;
+
+
+
+
+
